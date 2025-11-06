6a632ac4190bd01170646c39c92355c055a79da0
robbowland <me@robbow.land>
2025-11-06 19:39:35 +0000

    `gh-dash` formula for custom fork

---
+++ Formula/gh-dash.rb
@@ -0,0 +1,29 @@
+require "time"
+
+class GhDash < Formula
+  desc "GitHub CLI dashboard extension"
+  homepage "https://github.com/robbowland/gh-dash"
+  url "https://github.com/robbowland/gh-dash.git",
+      branch: "main",
+      revision: "53c8bc3adab3bd527f581609b17fe5d287f2f92b"
+  version "4.0.0-rb1"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/dlvhdr/gh-dash/v4/cmd.Version=#{version}
+      -X github.com/dlvhdr/gh-dash/v4/cmd.Commit=#{Utils.git_short_commit(revision)}
+      -X github.com/dlvhdr/gh-dash/v4/cmd.Date=#{Time.now.utc.iso8601}
+      -X github.com/dlvhdr/gh-dash/v4/cmd.BuiltBy=homebrew
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags)
+  end
+
+  test do
+    assert_match version, shell_output("#{bin}/gh-dash --version")
+  end
+end
