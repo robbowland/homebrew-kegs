1709fc3dd0fb72b192e869687a79bdf4a25fc676
robbowland <me@robbow.land>
2025-11-06 19:17:34 +0000

    `gitui` formula for custom fork

---
+++ Formula/gitui.rb
@@ -0,0 +1,71 @@
+class Gitui < Formula
+  desc "Blazing fast terminal-ui for git written in rust"
+  homepage "https://github.com/robbowland/gitui"
+  url "https://github.com/robbowland/gitui.git",
+      branch: "master",
+      revision: "ad4236881373262153c1d3cd8886c142c9972414"
+  version "0.27.0-rb1"
+  license "MIT"
+
+  depends_on "cmake" => :build # for libz-ng-sys
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+  depends_on "openssl@3"
+
+  uses_from_macos "zlib"
+
+  def install
+    # Ensure that the `openssl` crate picks up the intended library.
+    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
+    ENV["OPENSSL_NO_VENDOR"] = "1"
+
+    features = %w[
+      disable-log-files-tabs
+    ]
+
+    system "cargo", "install", "--features", features.join(","), *std_cargo_args
+  end
+
+  test do
+    require "utils/linkage"
+
+    system "git", "clone", "https://github.com/gitui-org/gitui.git"
+    (testpath/"gitui").cd { system "git", "checkout", "v0.7.0" }
+
+    input, _, wait_thr = Open3.popen2 "script -q screenlog.ansi"
+    input.puts "stty rows 80 cols 130"
+    input.puts "env LC_CTYPE=en_US.UTF-8 LANG=en_US.UTF-8 TERM=xterm #{bin}/gitui -d gitui"
+    sleep 2
+    # select log tab
+    input.puts "2"
+    sleep 2
+    # inspect commit (return + right arrow key)
+    input.puts "\r"
+    sleep 2
+    input.puts "\e[C"
+    sleep 2
+    input.close
+
+    screenlog = (testpath/"screenlog.ansi").read
+    # remove ANSI colors
+    screenlog.encode!("UTF-8", "binary",
+      invalid: :replace,
+      undef:   :replace,
+      replace: "")
+    screenlog.gsub!(/\e\[([;\d]+)?m/, "")
+    assert_match "Author: Stephan Dilly", screenlog
+    assert_match "Date: 2020-06-15", screenlog
+    assert_match "Sha: 9c2a31846c417d8775a346ceaf38e77b710d3aab", screenlog
+
+    linked_libraries = [
+      Formula["openssl@3"].opt_lib/shared_library("libcrypto"),
+      Formula["openssl@3"].opt_lib/shared_library("libssl"),
+    ]
+    linked_libraries.each do |library|
+      assert Utils.binary_linked_to_library?(bin/"gitui", library),
+             "No linkage with #{library.basename}! Cargo is likely using a vendored version."
+    end
+  ensure
+    Process.kill("TERM", wait_thr.pid)
+  end
+end
