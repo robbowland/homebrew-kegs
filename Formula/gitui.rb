6b1e0805e98e1dde61de028fa0112741b1632b88
Rob Bowland <me@robbow.land>
2025-11-09 21:47:19 +0000

    Update gitui version and revision

--- Formula/gitui.rb
+++ Formula/gitui.rb
@@ -3,8 +3,8 @@ class Gitui < Formula
   homepage "https://github.com/robbowland/gitui"
   url "https://github.com/robbowland/gitui.git",
       branch: "master",
-      revision: "9ef93e51d52025d0c7801d81cb4e92893aa4755c"
-  version "0.27.0-rb4"
+      revision: "5cbbde723c914f7abd107be4d8d8872353373dbf"
+  version "0.27.0-rb5"
   license "MIT"
 
   depends_on "cmake" => :build # for libz-ng-sys
