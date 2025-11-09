94734151ebd1497c6416236ae29401510960dba2
robbowland <me@robbow.land>
2025-11-09 14:32:00 +0000

    Add `serie` formula for personal fork

---
+++ Formula/serie.rb
@@ -0,0 +1,19 @@
+class Serie < Formula
+  desc "Rich git commit graph in your terminal"
+  homepage "https://github.com/robbowland/serie"
+  url "https://github.com/robbowland/serie.git",
+      branch: "master",
+      revision: "42637ac59348213a4837996a8e9e73816c61baa7"
+  version "0.5.3-rb1"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "serie 0.5.3", shell_output("#{bin}/serie --version")
+  end
+end
